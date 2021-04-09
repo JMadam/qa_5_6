da861acece6b7ba0ceef8eb0527dca97fa108d99
yuliyamadaminova <Cdtnjxrf2103>
2021-04-09 15:55:32 +0300

    Homework qa_guru_5_6

---
+++ src/test/java/tests/TxtFileText.java
@@ -0,0 +1,23 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import static utils.Files.readTextFromPath;
+
+public class TxtFileText {
+
+    @Test
+    void txtTest() throws IOException {
+        String txtFilePath = "./src/test/resources/files/1.txt";
+        String expectedData = "Hello, world";
+
+        String actualData = readTextFromPath(txtFilePath);
+
+        assertThat(actualData, containsString(expectedData));
+    }
+}
